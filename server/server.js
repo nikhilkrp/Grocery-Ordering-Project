cd39c1327e48e09d3dcb7b78a80d570cd6f21591
nikhilkrp <150312021+nikhilkrp@users.noreply.github.com>
2025-09-07 22:29:40 +0530

    frontend url added

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ app.use(express.json())
 
 // here we will add origin which can acccess this server
 app.use(cors({
-   origin:['http://localhost:5173'] ,
+   origin:['http://localhost:5173','https://grocery-ordering-project-bly3.vercel.app'] ,
    credentials:true
 }))
 
