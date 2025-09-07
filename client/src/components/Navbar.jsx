878f1485369f61948a11666ae6dbcbb9c09f4060
nikhilkrp <150312021+nikhilkrp@users.noreply.github.com>
2025-09-07 21:56:11 +0530

    resolved mobile view

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -44,14 +44,14 @@ const Navbar = () => {
 
             {/* Desktop Menu */}
             <div className="hidden sm:flex items-center gap-8">
-                <NavLink to="/">Home</NavLink>
-                <NavLink to="/products">All Products</NavLink>
-                <NavLink to="/contact">Contact</NavLink>
-                <NavLink to="/seller">Seller</NavLink>
+                <NavLink to="/" className="bg-green-400 hover:bg-green-500  px-2 rounded-lg text-white">Home</NavLink>
+                <NavLink className="bg-green-400 hover:bg-green-500  px-2 rounded-lg text-white" to="/products">Products</NavLink>
+                <NavLink className="bg-green-400 hover:bg-green-500  px-2 rounded-lg text-white" to="/contact">Contact</NavLink>
+                <NavLink className="bg-green-400 hover:bg-green-500  px-2 rounded-lg text-white" to="/seller">Seller</NavLink>
 
-                <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
-                    <input onChange={(e) => setSearchQuery(e.target.value)} className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" type="text" placeholder="Search products" />
-                    <img src={assets.search_icon} alt="seacrch" className="=w-4" />
+                <div className="hidden lg:flex items-center text-sm gap-2 border border-green-700 px-3 rounded-full">
+                    <input onChange={(e) => setSearchQuery(e.target.value)} className="py-1.5 w-full bg-transparent outline-none placeholder-green-500" type="text" placeholder="Search products" />
+                    <img className="text-green-900 w-4" src={assets.search_icon} alt="seacrch" />
                 </div>
 
                 <div onClick={() => navigate("/cart")} className="relative cursor-pointer">
@@ -68,8 +68,8 @@ const Navbar = () => {
                     ) : (
                         <div className="relative group">
                             <img src={assets.profile_icon} className="w-10" alt="" />
-                            <ul className="hidden group-hover:block top-10 right-0 bg-white shadow
-                        border border-gray-200 py-2.5 w-30 rounded-md text-sm z-40">
+                            <ul className="hidden absolute group-hover:block top-10 right-0 bg-white shadow
+                        border border-gray-200 py-2.5 w-40 rounded-md text-sm z-40">
                                 <li onClick={() => navigate("my-orders")} className="p=1.5 pl-3 hover:bg-red-400 cursor-pointer">My Orders</li>
                                 <li onClick={logout} className="p=1.5 pl-3 hover:bg-red-400 cursor-pointer">Logout</li>
                             </ul>
@@ -82,7 +82,7 @@ const Navbar = () => {
 
 
             {/* Mobile Menu */}
-            <div className="flex items-center gap-6 sm:hidden">
+            <div className="flex items-center gap-6 sm:hidden ">
                 <div onClick={() => navigate("/cart")} className="relative cursor-pointer">
                     <img src={assets.nav_cart_icon} alt="cart" className="h-4" />
                     <button className="absolute -top-2 -right-3 text-xs text-white bg-green-500 w-[18px] h-[18px] rounded-full">{getCartCount()}</button>
@@ -93,16 +93,16 @@ const Navbar = () => {
                 </button>
             </div>
             {open && (
-                <div className={`${open ? 'flex' : 'hidden'} absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden`}>
-                    <NavLink to="/" onClick={() => setOpen(false)}>Home</NavLink>
-                    <NavLink to="/products" onClick={() => setOpen(false)}>All Products</NavLink>
-                    {user && <NavLink to="/myorder" onClick={() => setOpen(false)}>My Orders</NavLink>}
-                    <NavLink to="/contact" onClick={() => setOpen(false)}>Contact</NavLink>
+                <div className={`${open ? 'flex' : 'hidden'} absolute top-[60px] right-0 w-full z-10 py-4 flex-col items-end gap-2 px-5 text-md md:hidden text-black opacity-90 `}>
+                    <NavLink  className="bg-green-400 hover:bg-green-500 px-2 rounded-lg text-white" to="/" onClick={() => setOpen(false)}>Home</NavLink>
+                    <NavLink className="bg-green-400 hover:bg-green-500  px-2 rounded-lg text-white" to="/products" onClick={() => setOpen(false)}>All Products</NavLink>
+                    {user && <NavLink className="bg-green-400 hover:bg-green-500  px-2 rounded-lg text-white" to="/myorder" onClick={() => setOpen(false)}>My Orders</NavLink>}
+                    <NavLink className="bg-green-400 hover:bg-green-500  px-2 rounded-lg text-white" to="/contact" onClick={() => setOpen(false)}>Contact</NavLink>
                     {!user ? (
-                        <button onClick={() => { setOpen(false); setShowUserLogin(true) }} className="cursor-pointer px-6 py-2 mt-2 bg-green-500 hover:bg-green-600 transition text-white rounded-full text-sm">
+                        <button onClick={() => { setOpen(false); setShowUserLogin(true) }} className="cursor-pointer px-4 py-2 mt-2 bg-red-500 hover:bg-red-600 transition text-white rounded-lg text-sm">
                             Login
                         </button>) : (
-                        <button onClick={logout} className="cursor-pointer px-6 py-2 mt-2 bg-green-500 hover:bg-green-600 transition text-white rounded-full text-sm">
+                        <button onClick={logout} className="cursor-pointer px-4 py-1 mt-2 bg-red-500 hover:bg-red-600 transition text-white rounded-lg text-sm">
                             Logout
                         </button>
                     )
