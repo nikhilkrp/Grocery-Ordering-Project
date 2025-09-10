7feb5bbd870ee625c4afb532e6dc0035908d66e7
nikhilkrp <150312021+nikhilkrp@users.noreply.github.com>
2025-09-10 23:19:19 +0530

    added online status functionality

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,8 +4,13 @@ import Categories from "../components/Categories"
 import Bestseller from "../components/Bestseller.jsx"
 import BottomBanner from "../components/BottomBanner.jsx"
 import Newsletter from "../components/Newsletter.jsx"
+import useOnlineStatus from "../utils/useOnlineStatus.jsx"
 
 const Home = () => {
+
+    const onlineStatus = useOnlineStatus();
+
+    if(onlineStatus===false)return <h1 className="text-red-600 flex items-center align-middle p-32 font-bold text-2xl" >Looks Like you are Offline</h1>
 
     return (
         <div className="mt-10">
